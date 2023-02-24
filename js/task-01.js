70d392d71eaf19c4ed572a935a39189a83657659
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-02-24 19:57:30 +0200

    finish 1, 2, 3 tasks

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const categoryAmount = document.querySelectorAll(".item");
+console.log("Number of categories:", categoryAmount.length);
+
+categoryAmount.forEach((category) => {
+  // console.log(category);
+  const categoryName = category.querySelector("h2");
+  console.log("\nCategory:", categoryName.textContent);
+
+  const categoryCount = category.querySelectorAll("li");
+  console.log("Elements:", categoryCount.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item");
+
+  ingredientsList.append(ingredientItem);
+});
