4820932b8e53fa2bab4dedb61ce92cf50ac43567
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-03-29 20:15:33 +0300

    delete console.log

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,37 @@
+// const inputEl = document.querySelector("#validation-input");
+// const inputElLength = Number(inputEl.dataset.length);
+
+// function handleInputEl() {
+//   if (inputEl.value.length === inputElLength) {
+//     inputEl.classList.add("valid");
+//     inputEl.classList.remove("invalid");
+//   } else {
+//     inputEl.classList.add("invalid");
+//     inputEl.classList.remove("valid");
+//   }
+// }
+
+// inputEl.addEventListener("blur", handleInputEl);
+
+// const onValidationValue = (event) => {
+//   const value = event.target.value;
+
+//   inputEl.classList.add("invalid");
+
+//   value.trim().length === inputElLength && value.trim() !== ""
+//     ? inputEl.classList.replace("invalid", "valid")
+//     : inputEl.classList.replace("valid", "invalid");
+// };
+// inputEl.addEventListener("blur", onValidationValue);
 
 const inputEl = document.querySelector("#validation-input");
 const inputElLength = Number(inputEl.dataset.length);
 
-function handleInputEl() {
-  if (inputEl.value.length === inputElLength) {
+function handleInputEl(event) {
+  inputEl.classList.add("invalid");
+  if (inputElLength === event.target.value.length) {
     inputEl.classList.add("valid");
     inputEl.classList.remove("invalid");
-  } else {
-    inputEl.classList.add("invalid");
-    inputEl.classList.remove("valid");
   }
 }
-
 inputEl.addEventListener("blur", handleInputEl);
--- js/task-08.js
+++ js/task-08.js
@@ -1,27 +1,44 @@
-const loginFormEl = document.querySelector(".login-form");
+// const loginFormEl = document.querySelector(".login-form");
 
-const inputFormEmailEl = loginFormEl.firstElementChild.firstElementChild;
+// const inputFormEmailEl = loginFormEl.firstElementChild.firstElementChild;
 
-const inputFormPassEl =
-  loginFormEl.firstElementChild.nextElementSibling.firstElementChild;
+// const inputFormPassEl =
+//   loginFormEl.firstElementChild.nextElementSibling.firstElementChild;
 
-const loginFormBtnEl = loginFormEl.lastElementChild;
+// const loginFormBtnEl = loginFormEl.lastElementChild;
 
-loginFormBtnEl.addEventListener("click", onFormSubmit);
+// loginFormBtnEl.addEventListener("click", onFormSubmit);
 
-function onFormSubmit() {
-  loginFormEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (inputFormEmailEl.value !== "" && inputFormPassEl.value !== "") {
-      const userData = {
-        email: inputFormEmailEl.value,
-        password: inputFormPassEl.value,
-      };
-      console.log(userData);
-      event.target.reset();
+// function onFormSubmit() {
+//   loginFormEl.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     if (inputFormEmailEl.value !== "" && inputFormPassEl.value !== "") {
+//       const userData = {
+//         email: inputFormEmailEl.value,
+//         password: inputFormPassEl.value,
+//       };
+//       console.log(userData);
+//       event.target.reset();
+//     } else {
+//       alert("Будь ласка, заповніть всі поля форми.");
+//     }
+//   });
+// }
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  const data = {};
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((val, name) => {
+    data[name] = val;
+    if (val === "") {
+      return window.alert("всі поля повинні бути заповнені");
     }
   });
-  if (inputFormEmailEl.value === "" || inputFormPassEl.value === "") {
-    alert("Будь ласка, заповніть всі поля форми.");
+  if (data.email !== "" && data.password !== "") {
+    form.reset();
   }
 }
